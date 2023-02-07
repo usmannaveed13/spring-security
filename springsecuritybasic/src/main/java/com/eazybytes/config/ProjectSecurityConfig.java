b445fdcb0755932e792984cff0b7c000d5ea57e4
Usman Naveed <usman.naveed@2iqresearch.com>
2023-02-07 17:04:09 +0500

    Define Users, Security using InMemoryUserDetailsManager,JdbcUserDetailsManager & UserDetailsService

--- springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.eazybytes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,8 +18,40 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
      * /notices - Not Secured
      * /contact - Not Secured
      */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+
+     @Override
+     protected void configure(HttpSecurity http) throws Exception {
+         http.authorizeRequests().antMatchers("/myAccount").authenticated().antMatchers("/myBalance").authenticated()
+                 .antMatchers("/myLoans").authenticated().antMatchers("/myCards").authenticated().antMatchers("/notices")
+                 .permitAll().antMatchers("/contact").permitAll().and().formLogin().and().httpBasic();
+     }
+
+    /*
+     * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+     * Exception {
+     * auth.inMemoryAuthentication().withUser("admin").password("12345").authorities
+     * ("admin").and(). withUser("user").password("12345").authorities("read").and()
+     * .passwordEncoder(NoOpPasswordEncoder.getInstance()); }
+     */
+
+    /*
+     * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+     * Exception { InMemoryUserDetailsManager userDetailsService = new
+     * InMemoryUserDetailsManager(); UserDetails user =
+     * User.withUsername("admin").password("12345").authorities("admin").build();
+     * UserDetails user1 =
+     * User.withUsername("user").password("12345").authorities("read").build();
+     * userDetailsService.createUser(user); userDetailsService.createUser(user1);
+     * auth.userDetailsService(userDetailsService); }
+     */
+
+    /*
+     * @Bean public UserDetailsService userDetailsService(DataSource dataSource) {
+     * return new JdbcUserDetailsManager(dataSource); }
+     */
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
 
         /**
          * Default configurations which will secure all the requests
@@ -52,10 +87,15 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
          * Configuration to permit all the requests
          */
 
-        http .authorizeRequests() .anyRequest().permitAll().and().formLogin().and()
-                .httpBasic();
+//        http .authorizeRequests() .anyRequest().permitAll().and().formLogin().and()
+//                .httpBasic();
 
 
+   // }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
